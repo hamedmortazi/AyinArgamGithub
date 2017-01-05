0b09a7e21a575aa256745f793c8354f626b7c922
hamed <hamed@DESKTOP-35I7DOJ>
2017-01-05 23:04:06 +0330

    951016

---
+++ Amoozesh/Amoozesh/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Amoozesh.Startup))]
+namespace Amoozesh
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
